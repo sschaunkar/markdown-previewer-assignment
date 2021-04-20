5d0044813ee62f5435c814ca4e10768a2ad5c728
sschaunkar <chhokar.sachin76@gmail.com>
2021-04-20 10:30:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.scss';
+import TextArea from './components/TextArea';
+
+function App() {
+  return (
+    <div className="App">
+     <TextArea />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextArea.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TextArea() {
+    return (
+        <div>
+            <textarea rows="10" col="10"></textarea>
+        </div>
+    )
+}
+
+export default TextArea
